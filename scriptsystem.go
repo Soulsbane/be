52052f15b626f7aa01a58b349047717f91173479
Paul Crane <paul@acheronsoft.com>
2019-08-31 01:23:32 -0500

    Hook up Commands struct to ScriptSystem.

--- scriptsystem.go
+++ scriptsystem.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	lua "github.com/yuin/gopher-lua"
+	luar "layeh.com/gopher-luar"
+)
+
 // ScriptSystem use Lua for scripting.
 type ScriptSystem struct {
+	state    *lua.LState
+	commands *Commands
+	blah     string
+}
+
+// NewScriptSystem Initializes the Lua Script System
+func NewScriptSystem() *ScriptSystem {
+	var scriptSystem ScriptSystem
+	scriptSystem.blah = "blah"
+	scriptSystem.state = lua.NewState()
+	scriptSystem.commands = NewCommands()
+	//defer state.Close()
+	scriptSystem.state.SetGlobal("Commands", luar.New(scriptSystem.state, scriptSystem.commands))
+
+	return &scriptSystem
 }
