089a2b98acd44e44191beffbaf8db7c85ca5d015
Paul Crane <paul@acheronsoft.com>
2019-09-03 02:22:54 -0500

    Add Load/Do methods to ScriptSystem.

--- launch.go
+++ launch.go
@@ -1,31 +1,19 @@
 package main
 
-import (
-	"fmt"
-
-	lua "github.com/yuin/gopher-lua"
-	luar "layeh.com/gopher-luar"
-)
+import "fmt"
 
 func main() {
-	L := lua.NewState()
-	defer L.Close()
-
 	const script = `print("Adding command from Lua") Commands:addCommand("subl", "subl")`
 
-	commands := NewCommands()
-	L.SetGlobal("Commands", luar.New(L, commands))
+	scriptSystem := NewScriptSystem()
 
-	commands.AddCommand("wow", "lsd")
-	commands.list()
+	scriptSystem.commands.AddCommand("wow", "lsd")
+	scriptSystem.commands.list()
 	fmt.Println("")
 
-	if err := L.DoString(script); err != nil {
-		panic(err)
-	}
-
-	commands.run("wow")
-	commands.run("subl")
+	scriptSystem.DoString(script)
+	scriptSystem.commands.run("wow")
+	scriptSystem.commands.run("subl")
 	fmt.Println("")
-	commands.list()
+	scriptSystem.commands.list()
 }
--- scriptsystem.go
+++ scriptsystem.go
@@ -9,17 +9,40 @@ import (
 type ScriptSystem struct {
 	state    *lua.LState
 	commands *Commands
-	blah     string
 }
 
 // NewScriptSystem Initializes the Lua Script System
 func NewScriptSystem() *ScriptSystem {
 	var scriptSystem ScriptSystem
-	scriptSystem.blah = "blah"
+
 	scriptSystem.state = lua.NewState()
 	scriptSystem.commands = NewCommands()
-	//defer state.Close()
 	scriptSystem.state.SetGlobal("Commands", luar.New(scriptSystem.state, scriptSystem.commands))
 
 	return &scriptSystem
+}
+
+// DestroyScriptSystem Calls lua.LState.Close
+func (s *ScriptSystem) DestroyScriptSystem() {
+	s.state.Close()
+}
+
+// DoString Run the passed code string
+func (s *ScriptSystem) DoString(code string) {
+	s.state.DoString(code)
+}
+
+// DoFile Load the file and run its code
+func (s *ScriptSystem) DoFile(fileName string) {
+	s.state.DoFile(fileName)
+}
+
+// LoadString load the passed code string
+func (s *ScriptSystem) LoadString(code string) (*lua.LFunction, error) {
+	return s.state.LoadString(code)
+}
+
+// LoadFile Load the file
+func (s *ScriptSystem) LoadFile(fileName string) (*lua.LFunction, error) {
+	return s.state.LoadFile(fileName)
 }
